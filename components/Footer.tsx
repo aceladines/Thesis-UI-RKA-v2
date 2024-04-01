393ec8899c9d5279202e1b996d97f939305605d3
aceladines <ajladines01@gmail.com>
2024-04-02 04:28:58 +0800

    [REFACTTOR] -  Changed the bottom position

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <h1 className="text-center text-xs font-extralight absolute bottom-0 inset-x-0">
+    <h1 className="text-center text-xs font-extralight absolute bottom-3 inset-x-0">
       Beatrice Escoto · Acemon Ladines · Abigail Macahilig
     </h1>
   );
